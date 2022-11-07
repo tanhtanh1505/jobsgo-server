404f5b098525b886667ff48764bcd51302eeb1f6
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-08 01:03:45 +0700

    fix: cors origin

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const server = http.createServer(app);
 
 setup.run();
 
-app.use(cors());
+// app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
