864c56ef975f1d5c2883e5ddb883f4416ec882c5
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-08 02:02:28 +0700

    fix: save cookie

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
