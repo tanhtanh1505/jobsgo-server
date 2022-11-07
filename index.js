0ac517b2d67f6c4df9d2ae6511ffdc12e3156100
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-08 01:01:37 +0700

    fix: cors origin

--- index.js
+++ index.js
@@ -27,9 +27,8 @@ setup.run();
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 app.use(cookieParser());
