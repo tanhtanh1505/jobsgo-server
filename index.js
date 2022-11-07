f8df01eef5eeb3007aeb3e27312d8102cb56fddc
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-08 00:42:42 +0700

    feat: cors

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ const server = http.createServer(app);
 setup.run();
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 
