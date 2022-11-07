bbe94ded97ee5905bc1ed15d8068b645f0fbab36
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-08 02:03:31 +0700

    fix: save cookie

--- index.js
+++ index.js
@@ -24,14 +24,8 @@ const server = http.createServer(app);
 
 setup.run();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    optionsSuccessStatus: 200,
-  })
-);
+// app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Content-Type", "application/json;charset=UTF-8");
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
