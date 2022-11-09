1fe1a892fdc785c5c872ba18d4c3efc5265b60bf
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-09 23:33:25 +0700

    feat: Add swagger upload file

--- routes/helper.js
+++ routes/helper.js
@@ -4,6 +4,27 @@ const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const catchAsync = require("../utils/catchAsync");
 
+/**
+ * @openapi
+ * /helper/upload:
+ *  post:
+ *      summary: Upload image to S3
+ *      tags:
+ *      - Helper
+ *      requestBody:
+ *          require: true
+ *          content:
+ *              multipart/form-data:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          file:
+ *                              type: string
+ *                              format: binary
+ *      responses:
+ *              200:
+ *                  description: success
+ */
 router.post("/upload", upload.single("file"), catchAsync(uploadFileToS3));
 
 module.exports = router;
