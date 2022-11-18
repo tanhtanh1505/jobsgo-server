dbdcee326fdaf699e83ea4aebdf5bc14aaa0d859
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-18 18:31:38 +0700

    fix: cv route add to user

--- controllers/cv.js
+++ controllers/cv.js
@@ -1,5 +1,7 @@
 const { uploadFile } = require("../helper/uploadImageHelper");
 const { createPDF } = require("../helper/saveToPdf");
+const JobseekerModel = require("../models/jobseeker");
+
 //save file to s3
 module.exports.create = async (req, res) => {
   const file = req.file;
@@ -14,5 +16,10 @@ module.exports.create = async (req, res) => {
 module.exports.generatePdf = async (req, res) => {
   const { cv } = req.body;
   const url = await createPDF({ cv: cv });
+  const jobseeker = await JobseekerModel.findOne({ id: req.user.id });
+  const oldCv = jobseeker.cv;
+  const newCv = oldCv + `,${url}`;
+  await JobseekerModel.update({ cv: newCv }, req.user.id);
+
   res.send(url);
 };
