91faed0f47a0a1ec974fafd339f1ce90a16acb19
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-11-20 23:34:29 +0700

    fix: allw all

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ setup.run();
 
 // app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
