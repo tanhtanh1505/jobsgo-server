c50c956bc48639433895255f56cf930c98355729
tanhtanh1505 <tanhtanh1505@gmail.com>
2022-12-11 17:49:02 +0700

    fix: Comment api inner user

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,17 +1,30 @@
 const CommentModel = require("../models/comment");
 const HttpException = require("../utils/HttpException");
+const UserModel = require("../models/user");
+const user = require("../constants/user");
 
 class CommentController {
+  innerCommentWithUser = async (comment) => {
+    const user = await UserModel.findOne({ id: comment.author });
+    const { password, ...userWithoutPassword } = user;
+    comment.user = userWithoutPassword;
+    return comment;
+  };
+
   // crud
   getComment = async (req, res) => {
     const result = await CommentModel.findOne({ id: req.params.commentId });
-    res.send(result);
+    res.send(await this.innerCommentWithUser(result));
   };
 
   getCommentOfEmployer = async (req, res) => {
     //id of job
     const result = await CommentModel.findCommentOfEmployer(req.params.id);
-    res.send(result);
+    const listComment = [];
+    for (let c of result) {
+      listComment.push(await this.innerCommentWithUser(c));
+    }
+    res.send(listComment);
   };
 
   createComment = async (req, res) => {
